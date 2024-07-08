ee29c969451073b9aa82277bce1f00004e342c24
Alok <aloks6449@gmail.com>
2024-07-09 00:02:45 +0530

    Third commit

---
+++ check_plaindrome.py
@@ -0,0 +1,11 @@
+# write a program to check palindrome
+
+def palindrome(s):
+    return s == s[::-1]
+
+s = input("Enter the String: ")
+print(s)
+if(palindrome(s)):
+    print(s, "is palindrome")
+else:
+    print(s, "is not palindrome.")
